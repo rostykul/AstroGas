b71ed5893c0e340401736271ddd792e0b0180c20
rostykul <rostyslav.kulnevsky@mail.utoronto.ca>
2019-12-30 01:15:17 -0500

    initial commit

---
+++ Scripts/scroll_script.js
@@ -0,0 +1,7 @@
+let scroll_el = document.getElementById("scrolling");
+
+window.addEventListener("scroll", () => {
+    scroll_el.style.setProperty("--scroll", window.pageYOffset);
+    //alert("value: " + window.pageYOffset);
+});
+//scroll_el.style.setProperty("margin", "50px");
